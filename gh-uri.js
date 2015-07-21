928a87ea42c8947bddb22d87ab746bd3c7a1b8f9
SoniEx2 <endermoneymod@gmail.com>
2015-07-21 20:04:25 -0300

    Broke it. Attempt to fix.

--- gh-uri.js
+++ gh-uri.js
@@ -1,6 +1,8 @@
-var uri = location.href.split("#").slice(1).join("#");
-if (uri !== "") {
-  document.getElementsByTagName("body")[0].innerHTML = "Attempting to open " + uri + "<br />"
-    + "Feel free to hit your browser's \"back\" button once you're done.";
-  window.open(uri, "_self");
+function dostuff() {
+  var uri = location.href.split("#").slice(1).join("#");
+  if (uri !== "") {
+    document.getElementsByTagName("body")[0].innerHTML = "Attempting to open " + uri + "<br />"
+      + "Feel free to hit your browser's \"back\" button once you're done.";
+    window.open(uri, "_self");
+  }
 }
