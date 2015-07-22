6b08b1217cca9785a28cfdbdd5eb23b6385ca3cb
SoniEx2 <endermoneymod@gmail.com>
2015-07-21 21:11:01 -0300

    Do it the right way

--- gh-uri.js
+++ gh-uri.js
@@ -1,8 +1,19 @@
 function dostuff() {
   var uri = location.href.split("#").slice(1).join("#");
   if (uri !== "") {
-    document.getElementsByTagName("body")[0].innerHTML = "Attempting to open " + uri + "<br />"
-      + "Feel free to hit your browser's \"back\" button once you're done.";
+    var prefix = document.createTextNode("Attempting to open ");
+    var uria = document.createElement("a");
+    uria.href = uri;
+    uria.appendChild(document.createTextNode(uri));
+    var suffix = document.createTextNode("Feel free to hit your browser's \"back\" button once you're done.");
+
+    var theBody = document.getElementsByTagName("body")[0];
+
+    theBody.appendChild(prefix);
+    theBody.appendChild(uria);
+    theBody.appendChild(document.createElement("br"));
+    theBody.appendChild(suffix);
+
     window.open(uri, "_self");
   }
 }
